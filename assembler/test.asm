f08854c1e6d8178aa08e4f24f04ececc66a61961
Michael Riegert <michael@eowyn.net>
2020-12-19 05:41:08 -0500

    Start of an assembler in python

---
+++ assembler/test.asm
@@ -0,0 +1,18 @@
+; no operation
+NOP
+
+; clear accumulator and carry
+CLB
+
+; increment register 15
+INC r15
+
+; jump to addr 0x31 if condition is 1
+JNT 0x31
+
+; fetch immediate from rom data 0x80 into register r5
+FIM r5 0x80
+
+; send contents of register pair r4 + r5 out as address
+; on A1 and A2 time in the instruction cycle
+JIN r3
